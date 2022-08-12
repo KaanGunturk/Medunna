96abb471234beab944c9f2aab14654e94b26fe85
Zeynep HAN <zeynephan9906@gmail.com>
2022-08-12 12:42:33 +0300

    ZH Son

--- src/test/java/pages/US_013_014Page.java
+++ src/test/java/pages/US_013_014Page.java
@@ -7,9 +7,9 @@ import utilities.Driver;
 
 import java.util.List;
 
-public class US_013_014 {
+public class US_013_014Page {
 
-    public US_013_014() {
+    public US_013_014Page() {
 
         PageFactory.initElements(Driver.getDriver(), this);
 
@@ -45,21 +45,57 @@ public class US_013_014 {
     @FindBy(xpath = "//span[@class='d-none d-md-inline']")
     public WebElement myAppointmentHastaEditButonu;
 
+    @FindBy(xpath = "(//span[@class='d-none d-md-inline'])[2]")
+    public WebElement statusDegistirilecekEditButonu;
+
+    @FindBy(xpath = "(//a[@class='btn btn-primary btn-sm'])[2]")
+    public WebElement showTestResultsTestOlanEditButonu2;
+
+    @FindBy(xpath = "//button[@id='save-entity']")
+    public WebElement statusDegisikliginiKaydedecekSubmit;
+
+
     @FindBy(xpath = "//a[@class='btn btn-danger btn-sm']")
     public WebElement showTestResultsButonu;
 
-    @FindBy(xpath = "//a[@class='btn btn-info btn-sm']")
+    @FindBy(xpath = "//a[@class='btn btn-info btn-sm']") //a[@class='btn btn-info btn-sm']//*[text()='View Results']
     public WebElement viewResults;
 
     //US13_TC01
-    @FindBy(xpath = "//thead//tr")
+    @FindBy(xpath = "//tbody//tr")
     public List <WebElement> viewResultsHastaBilgileri;
 
-    @FindBy(xpath = "//button[@class='btn btn-warning btn-sm']")
+    @FindBy(xpath = "//tbody//tr")
+    public List <WebElement> showTestResultsBilgileri;
+
+    @FindBy(xpath = "//thead//tr")
+    public List <WebElement> showTestResultsBaslikBilgileri;
+
+    @FindBy(xpath = "//*[text()='Request Inpatient']")
     public WebElement requestInpatientButonu;
 
     @FindBy(xpath = "//a[@class='btn btn-primary btn-sm']")
     public WebElement myInpatientsEditButonu;
+
+    @FindBy(xpath = "//div[text()='InPatient request already done for this appointment']")
+    public WebElement inpatientMessage;
+
+    @FindBy(xpath = "//div[text()='A new In Patient is created with identifier']")
+    public WebElement inpatientSuccessfulyMessage;
+
+    @FindBy(xpath = "//*[text()='Physician Team54']")
+    public WebElement physicianTeam54Butonu;
+
+    @FindBy(xpath = "(//a[@class='dropdown-item'])[6]")
+    public WebElement signOut;
+
+    @FindBy(xpath = "//button[@class='btn btn-primary']")
+    public WebElement saveButton;
+
+    @FindBy(xpath = "//div[@role='alert']")
+    public WebElement saveHataMesaji;
+
+
 
 
 
