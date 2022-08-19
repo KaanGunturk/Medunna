9e530096c931bfc9c201f852a6c5cb10e12091fb
Ayberk Gürfidan <ayberk546@gmail.com>
2022-08-19 17:40:35 +0300

    database utils added

---
+++ src/test/java/pojos/ortakPojo/TestItem.java
@@ -0,0 +1,75 @@
+package pojos.ortakPojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TestItem {
+
+    private String name;
+    private String description;
+    private int price;
+    private String defaultValMin;
+    private String defaultValMax;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDefaultValMin() {
+        return defaultValMin;
+    }
+
+    public void setDefaultValMin(String defaultValMin) {
+        this.defaultValMin = defaultValMin;
+    }
+
+    public String getDefaultValMax() {
+        return defaultValMax;
+    }
+
+    public void setDefaultValMax(String defaultValMax) {
+        this.defaultValMax = defaultValMax;
+    }
+
+    public TestItem() {
+    }
+
+    public TestItem(String name, String description, int price, String defaultValMin, String defaultValMax) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.defaultValMin = defaultValMin;
+        this.defaultValMax = defaultValMax;
+    }
+
+    @Override
+    public String toString() {
+        return "TestItem{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", defaultValMin='" + defaultValMin + '\'' +
+                ", defaultValMax='" + defaultValMax + '\'' +
+                '}';
+    }
+}
